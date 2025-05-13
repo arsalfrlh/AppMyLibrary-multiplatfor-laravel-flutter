2c5d209dc0be51cfe34d430a01c5a64b7db0efe8
Kwanzaa <144583426+arsalfrlh@users.noreply.github.com>
2025-05-14 06:54:45 +0700

    Update buku.dart

--- Frontend/perpus/lib/models/buku.dart
+++ Frontend/perpus/lib/models/buku.dart
@@ -1,13 +1,13 @@
 class Buku {
-  final int id;
+  final int id; //objek yg di kembalikan atw di buat akan di simpan di variabel ini
   final String? gambar;
   final String judul;
   final String penulis;
   final int stok;
 
-  Buku({required this.id, this.gambar, required this.judul, required this.penulis, required this.stok});
+  Buku({required this.id, this.gambar, required this.judul, required this.penulis, required this.stok}); //construktor utk membuat objek |digunakan seperti create, update
 
-  factory Buku.fromJson(Map<String, dynamic> json){
+  factory Buku.fromJson(Map<String, dynamic> json){ //factory named construktor yg digunakan untuk mengembalikan objek yg sudah ada| digunakan utk mengembalikan objek dari api service
     return Buku(
       id: json['id'],
       gambar: json['gambar'],
